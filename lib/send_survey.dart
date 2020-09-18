606a5bcbda3c416efcb27ba868bf9aca7de1a3f5
nnnlog <sorisem4106@naver.com>
2020-09-18 12:33:28 +0900

    Release v0.2.0, 알림 센터 지원, 최신 버전 확인

--- lib/send_survey.dart
+++ lib/send_survey.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:jaga_jindan/rsa_encrypt.dart';
 import 'package:http/http.dart' as http;
 
@@ -18,9 +19,50 @@ void toast(String message) {
       fontSize: 12.0);
 }
 
+Future onDidReceiveLocalNotification(
+    int id, String title, String body, String payload) async {
+  //toast(body + "|" + payload);
+}
+
+Future selectNotification(String payload) async {
+  if (payload != null) {
+    debugPrint('notification payload: ' + payload);
+    toast(payload);
+  }
+}
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+var initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+var initializationSettingsIOS = IOSInitializationSettings(
+    onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+var initializationSettings = InitializationSettings(
+    initializationSettingsAndroid, initializationSettingsIOS);
+var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'com.nlog.flutterlocalnotifications.ScheduledNotificationBootReceiver',
+    '자가진단 자동화',
+    '자동으로 자가진단 설문을 제출합니다.',
+    importance: Importance.Max,
+    priority: Priority.High,
+    ticker: 'ticker');
+var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+var platformChannelSpecifics = NotificationDetails(
+    androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+
+void initNotification() async {
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: selectNotification);
+}
+
+var notiId = 0;
+void noti(String title, String desc) async {
+  await flutterLocalNotificationsPlugin
+      .show(notiId++, title, desc, platformChannelSpecifics, payload: desc);
+}
+
 class JagaJindanData {
   String name, birthday, school, edu, password;
-  bool force, agree = false, startup = false;
+  bool force, agree = false, startup = false, noti = false;
 
   static JagaJindanData readFromJSON(dynamic json) {
     return new JagaJindanData(
@@ -31,11 +73,12 @@ class JagaJindanData {
         json["password"] ?? "",
         json["force"] ?? false,
         json["agree"] ?? false,
-        json["startup"] ?? false);
+        json["startup"] ?? false,
+        json["noti"] ?? false);
   }
 
   JagaJindanData(this.name, this.birthday, this.school, this.edu, this.password,
-      this.force, this.agree, this.startup);
+      this.force, this.agree, this.startup, this.noti);
 
   dynamic toJSON() {
     return {
@@ -46,23 +89,29 @@ class JagaJindanData {
       "password": this.password,
       "force": this.force,
       "agree": this.agree,
-      "startup": this.startup
+      "startup": this.startup,
+      "noti": this.noti
     };
   }
 }
 
+void showSurveyResult(bool success, String message, JagaJindanData credentials) {
+  if (credentials.noti) noti(success ? "자가진단 제출을 성공하였습니다." : "자가진단 제출을 실패하였습니다.", message);
+  else toast(message);
+}
+
 void sendSurvey(JagaJindanData credentials) async {
   try {
-    String jwt =
-        jsonDecode((await http.post('https://${credentials.edu}hcs.eduro.go.kr/loginwithschool',
-                body: jsonEncode({
-                  'birthday': encrypt(credentials.birthday),
-                  'name': encrypt(credentials.name),
-                  'orgcode': credentials.school
-                }),
-                headers: {'Content-Type': 'application/json'},
-                encoding: Encoding.getByName('utf-8')))
-            .body)['token'];
+    String jwt = jsonDecode((await http.post(
+            'https://${credentials.edu}hcs.eduro.go.kr/loginwithschool',
+            body: jsonEncode({
+              'birthday': encrypt(credentials.birthday),
+              'name': encrypt(credentials.name),
+              'orgcode': credentials.school
+            }),
+            headers: {'Content-Type': 'application/json'},
+            encoding: Encoding.getByName('utf-8')))
+        .body)['token'];
 
     if (!credentials.force) {
       if ((await http.post('https://${credentials.edu}hcs.eduro.go.kr/checkpw',
@@ -73,11 +122,12 @@ void sendSurvey(JagaJindanData credentials) async {
               }))
               .statusCode !=
           200) {
-        toast('자가진단 페이지에서 초기 비밀번호를 설정하세요.');
+        showSurveyResult(false, '자가진단 페이지에서 초기 비밀번호를 설정하세요.', credentials);
         return;
       }
 
-      if (jsonDecode((await http.post('https://${credentials.edu}hcs.eduro.go.kr/secondlogin',
+      if (jsonDecode((await http.post(
+                  'https://${credentials.edu}hcs.eduro.go.kr/secondlogin',
                   body: jsonEncode({
                     'deviceUuid': '',
                     'password': encrypt(credentials.password)
@@ -88,7 +138,7 @@ void sendSurvey(JagaJindanData credentials) async {
               }))
               .body)['isError'] ==
           true) {
-        toast('비밀번호를 잘못 입력했습니다.');
+        showSurveyResult(false, '비밀번호를 잘못 입력했습니다.', credentials);
         return;
       }
     }
@@ -107,7 +157,8 @@ void sendSurvey(JagaJindanData credentials) async {
     var userNo = int.parse(users['groupList'][0]['userPNo']);
     String org = users['groupList'][0]['orgCode'];
 
-    jwt = jsonDecode((await http.post('https://${credentials.edu}hcs.eduro.go.kr/userrefresh',
+    jwt = jsonDecode((await http.post(
+            'https://${credentials.edu}hcs.eduro.go.kr/userrefresh',
             body: jsonEncode({'userPNo': userNo, 'orgCode': org}),
             headers: {
           'Authorization': jwt,
@@ -115,7 +166,8 @@ void sendSurvey(JagaJindanData credentials) async {
         }))
         .body)['UserInfo']['token'];
 
-    var res = await http.post('https://${credentials.edu}hcs.eduro.go.kr/registerServey',
+    var res = await http.post(
+        'https://${credentials.edu}hcs.eduro.go.kr/registerServey',
         body: jsonEncode({
           'rspns01': '1',
           'rspns02': '1',
@@ -137,9 +189,9 @@ void sendSurvey(JagaJindanData credentials) async {
         }),
         headers: {'Authorization': jwt, 'Content-Type': 'application/json'});
 
-    toast("자가진단 설문이 ${DateTime.now().toString().substring(0, 19)}에 제출되었습니다.");
+    showSurveyResult(true, "자가진단 설문이 ${DateTime.now().toString().substring(0, 19)}에 제출되었습니다.", credentials);
   } catch (e) {
-    toast("인증 정보를 한번 더 확인해주세요.\n오류가 계속 발생하는 경우 개발자에게 알려주세요.");
+    showSurveyResult(false, "인증 정보를 한번 더 확인해주세요.\n오류가 계속 발생하는 경우 개발자에게 알려주세요.", credentials);
     //toast(e.toString());
   }
 }
